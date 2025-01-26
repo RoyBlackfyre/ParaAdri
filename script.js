59b5c909fc46ad8ee22e30dcd1f9755f61a99578
Roel U <roelul@outlook.com>
2025-01-25 20:26:35 -0700

    Add buttons and style to them

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const root = document.documentElement;
+
+root.addEventListener("mousemove", e=>{
+    root.style.setProperty('--mouse-x', e.clientX + "px")
+    root.style.setProperty('--mouse-y', e.clientY + "px")
+
+})
