b41499c5fbee96557f59e145f988dd9644c67294
Roel U <roelul@outlook.com>
2025-01-26 21:58:49 -0700

    Proyecto medioterminaido

--- script.js
+++ script.js
@@ -1,8 +1,42 @@
 
 const root = document.documentElement;
+const body = document.body;
+const teamo=document.querySelector("#teamo")
 
-root.addEventListener("mousemove", e=>{
-    root.style.setProperty('--mouse-x', e.clientX + "px")
-    root.style.setProperty('--mouse-y', e.clientY + "px")
+let generador = 0;
+
+root.addEventListener("click", e=>{
+   if(e.target.classList.value =="no"){
+    alert("a")
+   }
+   if(e.target.classList.value =="si" && generador <6){
+    e.target.classList = "no";
+    e.target.innerText = "NO";
+   
+    generateSiButton(generador)
+   } else {
+        teamo.style.display="block"
+   }
 
 })
+
+
+function generateSiButton(intento){
+    let x = (generarNumeroRandom(0,310)).toString();
+    let y = (generarNumeroRandom(148,700)).toString()
+
+    generador ++
+    let newButton = document.createElement('button');
+    newButton.textContent = "Si";
+    newButton.classList = "si";
+    newButton.style.position = "absolute";
+    newButton.style.left = x + "px"
+    newButton.style.top = y + "px"
+
+    body.appendChild(newButton)
+}
+
+function generarNumeroRandom(min,max){
+    return Math.floor(Math.random() * (max-min+1)) 
+}
+
